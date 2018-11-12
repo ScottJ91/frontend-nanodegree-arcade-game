d9301faece913c1c1141c6cbbb87ef8ab70a019b
Scott Jensen <scottj91.sj@gmail.com>
2018-11-11 19:55:20 -0700

    Update js/app.js
    player class added

--- js/app.js
+++ js/app.js
@@ -38,23 +38,26 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y) {
+        this.x = x;
+        this.y = y;
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+        // Now instantiate your objects.
+        // Place all enemy objects in an array called allEnemies
+        // Place the player object in a variable called player
 
 
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
-    };
+        // This listens for key presses and sends the keys to your
+        // Player.handleInput() method. You don't need to modify this.
+        document.addEventListener('keyup', function(e) {
+            var allowedKeys = {
+                37: 'left',
+                38: 'up',
+                39: 'right',
+                40: 'down'
+            };
 
-    player.handleInput(allowedKeys[e.keyCode]);
-});
+            player.handleInput(allowedKeys[e.keyCode]);
+        });
